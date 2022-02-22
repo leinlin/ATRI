54d105990ac8d0b37b9092306e36c5a0c4574281
leinlin <leinlin@tencent.com>
2022-02-22 19:01:21 +0800

    加密

--- ATRI/plugins/util/__init__.py
+++ ATRI/plugins/util/__init__.py
@@ -43,8 +43,9 @@ async def _ready_en(matcher: Matcher, args: Message = CommandArg()):
 @encrypt_en.got("encr_en_text", "内容呢？！")
 async def _deal_en(text: str = ArgPlainText("encr_en_text")):
     is_ok = len(text)
-    if is_ok < 1:
-        await encrypt_en.reject("太短不加密！")
+    if is_ok < 10:
+        text = text + "          "
+        #await encrypt_en.reject("太短不加密！")
     en = Encrypt()
     result = en.encode(text)
     await encrypt_en.finish(result)
